2a3e82168c6bb2d53ddcae927e4468a9fde6b0ad
Tom Richards <tom@tomrichards.net>
2020-07-14 22:22:07 -0400

    Remove init script

--- script/build.sh
+++ script/build.sh
@@ -18,6 +18,5 @@ fpm \
 	-C /tmp/installdir \
 	-p mausberry-switch_VERSION_ARCH.deb \
 	-d "libglib2.0-0 >= 2.3" \
-	--deb-init data/mausberry-switch.init \
 	--deb-systemd data/mausberry-switch.service \
 	usr/bin etc
