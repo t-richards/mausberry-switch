0b043e767f0945ae5f477f6acc856e7ddad2e3ac
Tom Richards <tom@tomrichards.net>
2020-12-25 15:28:00 -0500

    Rename test

--- tests/test_maus.c
+++ tests/test_maus.c
@@ -2,7 +2,7 @@
 #include <locale.h>
 #include <stdlib.h>
 
-void test_exit_non_zero_with_invalid_configuration_file()
+void test_exit_non_zero_without_gpio()
 {
     int exitstatus = system("../src/mausberry-switch");
     g_assert_cmpint(WEXITSTATUS(exitstatus), ==, 1);
@@ -16,9 +16,8 @@ main (int argc, char *argv[])
     g_test_init(&argc, &argv, NULL);
     g_test_bug_base("https://github.com/t-richards/mausberry-switch/issues");
 
-    // Define the tests.
     g_test_add_func("/libmaus/test_exit_nonzero",
-                    test_exit_non_zero_with_invalid_configuration_file);
+                    test_exit_non_zero_without_gpio);
 
     return g_test_run();
 }
